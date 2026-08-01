eeedef388161f907c2b2d0a4e53e207aff4d4a66
HajarSlimani <hajarslimani484@gmail.com>
2026-08-01 23:31:16 +0100

    Update ExperienceController/add @PreAuthorize Admin

--- backend/src/main/java/ma/lbledfirst/backend/controller/ExperienceController.java
+++ backend/src/main/java/ma/lbledfirst/backend/controller/ExperienceController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import ma.lbledfirst.backend.domain.Experience;
 import ma.lbledfirst.backend.service.ExperienceService;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,22 +35,26 @@ public class ExperienceController {
         return service.findById(id);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public Experience create(@RequestBody Experience experience) {
         return service.save(experience);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public Experience update(@PathVariable Long id, @RequestBody Experience experience) {
         experience.setId(id);
         return service.update(id, experience);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}/publish")
-public Experience publish(@PathVariable Long id) {
+    public Experience publish(@PathVariable Long id) {
     return service.publish(id);
-}
-
+    }
+    
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);
